c3cc1397da876646248d63c04e30502b68017f44
SosnowskaJesika <sosnowska.j97@gmail.com>
2023-05-24 01:17:02 +0200

    update

--- train_model.py
+++ train_model.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
-df = pd.read_csv('data/prepared_data.csv')
+df = pd.read_csv('prepared_data.csv')
 
 X_train, X_test, y_train, y_test = train_test_split(df.drop(['rating'], axis=1), df['rating'], test_size=0.2, random_state=42)
 
@@ -15,5 +15,5 @@ model.compile(optimizer='RMSprop', loss='mse', metrics=['mae', 'mse'])
 
 model.fit(X_train, y_train, batch_size=32, epochs=100, validation_split=0.2)
 
-model_filename = 'models/trained_model.pkl'
+model_filename = 'trained_model.pkl'
 model.save(model_filename)
