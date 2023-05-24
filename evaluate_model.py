bc09b43cb91fe841b04048f76341fed090144a1c
SosnowskaJesika <sosnowska.j97@gmail.com>
2023-05-24 02:30:03 +0200

    update

--- evaluate_model.py
+++ evaluate_model.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from sklearn.metrics import accuracy_score
-from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 import tensorflow as tf
@@ -17,9 +16,8 @@ model = tf.keras.models.load_model('model.h5')
 
 # Ewaluacja modelu
 y_pred = model.predict(X)
-y_pred_classes = np.argmax(y_pred, axis=1)
+y_pred_classes = np.argmax(y_pred, axis=-1)
 
 accuracy = accuracy_score(y, y_pred_classes)
 
 print(f'Accuracy: {accuracy}')
-print(f'Mean Absolute Error: {mae}')
